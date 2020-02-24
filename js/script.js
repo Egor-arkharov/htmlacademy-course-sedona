67da428f8d2c97c00a6722d6e42edadcfb62a3ec
Egor-arkharov <59442265+Egor-arkharov@users.noreply.github.com>
2020-02-25 01:31:13 +0300

    Правки 6
    - Обертка всего контента в див
    
    - В IE11 верстка списка отелей и блока с сортировкой сильно едет, не пойму почему(

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let button = document.querySelector(".search");
+let popup = document.querySelector(".find-hotel__form");
+
+button.addEventListener("click", function (evt) {
+  console.log('click')
+  popup.classList.toggle("form-show");
+});
